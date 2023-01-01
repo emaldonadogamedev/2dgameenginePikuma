d3d1dade9e541eaf5a52602ebb40ed6d0c60a486
emaldonadogamedev <em_gamedev@outlook.com>
2023-01-01 15:51:51 -0400

    starting to work on game class

--- 2dgameengine/src/Main.cpp
+++ 2dgameengine/src/Main.cpp
@@ -1,24 +1,13 @@
-#include <iostream>
+#include "Game.h"
 
-#include <SDL2/SDL.h> // main SDL features
-#include <SDL2/SDL_image.h> // working with images
-#include <SDL2/SDL_ttf.h> // for dealing with true-type fonts
-#include <SDL2/SDL_mixer.h> // for dealing with audio
+int main(int argc, char* argv[])
+{   
+    Game game;
 
-#include <glm/glm.hpp> // base glm file for the cool math
+    game.Initialize();
+    game.Run();
 
-#include <imgui/imgui.h> // UI and debug UI
-
-#include <sol/sol.hpp> // for binding Lua to C++
-
-int main()
-{
-    sol::state lua;
-    lua.open_libraries(sol::lib::base);
-
-    SDL_Init(SDL_INIT_EVERYTHING);
-
-    std::cout << "Hello, world!" << std::endl;
+    game.Destroy();
 
     return 0;
 }
